de818deb7c01b37c553067b841a8ab3407777131
Alex <alex.kojokari42@gmail.com>
2022-09-09 14:06:58 +0300

    Изменил файл README.md

--- Array_1-2/Program.cs
+++ Array_1-2/Program.cs
@@ -7,20 +7,10 @@
 
 int[] Array1 = { 10, 13, 42, 69, 113, 25 };
 int n1 = Array1.Length;
-int index1 = 0;
-
-System.Console.WriteLine("This is the first array");
-while (index1 < n1)
-{
-    System.Console.Write($"{Array1[index1]} ");
-    index1++;
-}
-
-System.Console.WriteLine();
 
 int index2 = 2;
-Random rand = new Random();
-int index = rand.Next(Array1.Length);
+Random Rand = new Random();
+int index = Rand.Next(Array1.Length);
 
 System.Console.WriteLine("This is the second array");
 while (index <= index2)
