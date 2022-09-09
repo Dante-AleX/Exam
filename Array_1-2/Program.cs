f7a4b6d23d18105b79178b28818795035f136b76
Alex <alex.kojokari42@gmail.com>
2022-09-09 13:49:29 +0300

    Добавил второй массив и решение задачи

--- Array_1-2/Program.cs
+++ Array_1-2/Program.cs
@@ -6,13 +6,25 @@
 */
 
 int[] Array1 = { 10, 13, 42, 69, 113, 25 };
-int n = Array1.Length;
-int index = 0;
+int n1 = Array1.Length;
+int index1 = 0;
 
 System.Console.WriteLine("This is the first array");
-while (index < n)
+while (index1 < n1)
 {
-    System.Console.Write($"{Array1[index]} ");
+    System.Console.Write($"{Array1[index1]} ");
+    index1++;
+}
+
+System.Console.WriteLine();
+
+int index2 = 2;
+Random rand = new Random();
+int index = rand.Next(Array1.Length);
+
+System.Console.WriteLine("This is the second array");
+while (index <= index2)
+{
+    Console.Write($"{Array1[index]} ");
     index++;
 }
-System.Console.WriteLine();
